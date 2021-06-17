13fa85b5b1b5e74ec79e83ab4a522b70a6636f98
vknayak <kumar19@navgurukul.org>
2021-06-17 12:33:00 +0530

    changes

--- Project6/script.js
+++ Project6/script.js
@@ -1,59 +1,24 @@
-function addBtn(){
-    const inputBox= document.getElementById("input1");
-    console.log(inputBox.value)
-    let userData= inputBox.value;
-    let getLocalStorage=localStorage.getItem("New Todo");
-    if (getLocalStorage == null){
-        listArr=[]
-    }
-    else{
-        listArr = JSON.parse(getLocalStorage);
-    }
-    listArr.push(userData);
-    localStorage.setItem("New Todo",JSON.stringify(listArr));
-    showTasks();
-}
+// Function1, write a function addBtn which do sub tasks mention below
+//  - get the input1 value whatever the user is enetering for task
+//  - get the local storage values by getItem function if it is null create a new list or parse it after getting from local storage into normal list
+//  - push the new task that you got from input1 and set that in localstorage by using setItem function
+//  - and show all taks after adding with the new lsit that has all the todos by calling the function which is responsible for showing tasks
 
-function showTasks(){
-    const inputBox= document.getElementById("input1");
-    const todoList= document.getElementById("todoList")
-    const footer= document.getElementById("footer")
-    let getLocalStorage=localStorage.getItem("New Todo");
-    console.log(getLocalStorage);
-    if (getLocalStorage == null){
-        listArr=[]
-    }
-    else{
-        listArr= JSON.parse(getLocalStorage);
-    }
-    let newTagLi=""
-    listArr.forEach((element,index) => {
-        newTagLi+= `<li>${element} <button style="color:red;background-color:black ;" onclick="deleteTodo(${index})"><i>X</i></button></li>`
-    });
-    todoList.innerHTML=newTagLi
-    footer.innerHTML=`you have ${listArr.length} tasks`
-    inputBox.value="";
-}
+// Showtasks, write a function ShowTasks is which will do
+//  - get the input1 value whatever the user is enetering for task
+//  - get the todolist value where you want to save all the todos by writing
+//  - get the footer value for writing the how many tasks that you have
+//  - get the local storage values by getItem function if it is null create a new list or parse it after getting from local storage into normal list
+//  - After getting all tods from localstorage we need to run a loop for creating a list of tasks that defines each task with a button of deeleteTodo. deleteTodo function which will take 1 parameter of the index of each task
+//  - Update the todolist value with all lis that we made
+//  - Update footer value with the total task that we have and say them that you have these many tasks todo
+//  - and update the input1 value means that input box value should to empty
 
-function clearAll(){
-    let getLocalStorage=localStorage.removeItem("New Todo");
-    const todoList= document.getElementById("todoList")
-    const footer= document.getElementById("footer")
-    if (getLocalStorage == null){
-        listArr=[]
-    }
-    else{
-        listArr= JSON.parse(getLocalStorage);
-    }
-    let newTagLi=""
-    todoList.innerHTML=newTagLi
-    footer.innerHTML=`you have ${listArr.length} tasks`
-}
 
-function deleteTodo(index){
-    let getLocalStorage=localStorage.getItem("New Todo");
-    listArr= JSON.parse(getLocalStorage);
-    listArr.splice(index,1);
-    localStorage.setItem("New Todo", JSON.stringify(listArr));
-    showTasks();
-}
+// Fucntion clearALl, which will do 
+//  - clear all todos by emptying the localstorage values by using removeItem
+//  - and update the footer value also to zero tasks because after deleting there are no tasks to do
+//  - And no list items means not tasks need to show in web page
+
+
+// Function deleteTodo, which takes 1 parameter of index value from the task and use that index to find the task and delete that task and show remaong tasks
