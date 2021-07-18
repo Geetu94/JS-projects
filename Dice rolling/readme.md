a5e8ec390af9049658cb75503eb2ed33f5bbbd15
vknayak <59567895+vknayak@users.noreply.github.com>
2021-07-18 11:55:18 +0530

    Add files via upload

---
+++ Dice rolling/readme.md
@@ -0,0 +1,13 @@
+Dice rolling
+
+https://codepen.io/Lazyboy/pen/avjgEj
+
+1.In google search for the Dice rolling application 
+2.View the different applications
+3.And use your own creativity for creating a dice rollinng application where 
+dice will show 1 to 6 randomly after rolling dice
+4.1 small application you can do this on console too. if you want to make a complex application by the understanding of css/Javascript.
+5. After completing the code show to your peers and take feedback
+
+You can make two dice too, this could may design to paly with your co friend.
+
---
+++ Quotes World/script.js
@@ -0,0 +1,63 @@
+// Quotes list has the dictionaries of each quote details with the keys as quote, author, link
+
+var quotes = [
+	{
+		"quote": "Be the change you wish to see.",
+		"author": "Mahatma Gandhi",
+		"link": "https://en.wikipedia.org/wiki/Mahatma_Gandhi"
+	},
+	{
+		"quote": "Social progress can be measured by the social position of the female sex.",
+		"author": "Karl Marx",
+		"link": "https://en.wikipedia.org/wiki/Karl_Marx"
+	},
+	{
+		"quote": "I like the religion that teaches liberty, equality and fraternity.",
+		"author": "B. R. Ambedkar",
+		"link": "https://en.wikipedia.org/wiki/B._R._Ambedkar"
+	},
+	{
+		"quote": "If you tremble with indignation at every injustice then you are a comrade of mine.",
+		"author": "Ernesto Che Guevara",
+		"link": "https://en.wikipedia.org/wiki/Che_Guevara"
+	},
+	{
+		"quote": "Teaching is a very noble profession that shapes the character, caliber, and future of an individual. If the people remember me as a good teacher, that will be the biggest honour for me.",
+		"author": "Abdul Kalam",
+		"link": "https://en.wikipedia.org/wiki/A._P._J._Abdul_Kalam"
+	}
+];
+
+// list of colors to show as background colors
+var bgColors = ["red", "pink", "yellow", "green", "white", "darkcyan", "silver", "BlanchedAlmond", "chocolate", "darkorchid"];
+
+
+//  on starting of window we will need to satrt with 1 random quote and author and link of him
+//  for that we will use onload in window with a function named start as a value a
+// function start will do set the Value of saidso, author, link and add background colors to main and quote division
+
+
+window.onload = function start(){
+	document.getElementById("saidso").innerHTML = quotes[0].quote;
+	document.getElementById("author").innerHTML = quotes[0].author;
+	document.getElementById("main").style.backgroundColor = bgColors[0];
+	document.getElementById("quote").style.backgroundColor = bgColors[1];
+	document.getElementById("link").href = quotes[0].link
+}
+
+
+// Function changeQuote will work when the user click the button kuch aur and it will
+// change the quote randomly from a list, and the colors of main and quote divisons
+
+function changeQuote() {
+	
+  var i = Math.floor(Math.random()*5);
+  var j = Math.floor(Math.random()* bgColors.length);
+  var k = Math.floor(Math.random()* bgColors.length);
+  
+  document.getElementById("saidso").innerHTML = quotes[i].quote;
+  document.getElementById("author").innerHTML = quotes[i].author;
+  document.getElementById("main").style.backgroundColor = bgColors[j];
+  document.getElementById("quote").style.backgroundColor = bgColors[k];
+  document.getElementById("link").href = quotes[i].link
+}
