85b8d3f82377676377d23a5bce48b43fee24c73f
Geetu Sharma <95202700+Geetu94@users.noreply.github.com>
2022-03-12 13:26:26 +0530

    Delete Project4 directory

--- Project4/script.js
+++
@@ -1,7 +0,0 @@
-// write a function named changeColor  
-    // - get the container1 color value by using getElementById and store it in a variable
-    // - get the input1 value by using getElementById and store it in a variable
-    // - update the background color of container1 by the input1 value
-    // - And clear the input that user enter to empty value
-
-    
